6bfda6352a261ccf22b53d815bccab36c4167897
R <robsonpn93@gmail.com>
2018-02-21 15:20:10 -0300

    controle

--- calcula_inss.py
+++ calcula_inss.py
@@ -1,8 +1,14 @@
-def calcula_inss (salario):
+# funcao que calcula o inss 
+def calcular_inss (salario):
     imposto_inss = int (salario * 20 / 100)
     return imposto_inss
 
 salario = 3000
-imposto_inss= calcula_inss(salario)
+imposto_inss= calcular_inss(salario)
+
+#funcao que calcula o desconto dos dependentes
+
+def desconto_dependentes (dependente):
+    num_dependente
 
 print ("valor do imposto e: ", imposto_inss)
